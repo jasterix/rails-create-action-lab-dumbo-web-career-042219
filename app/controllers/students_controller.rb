f3fcc0979a0564686ab90496376b42ee67fac9ce
Jasterix C <loune.calixte@gmail.com>
2019-06-02 03:33:56 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create
+    @student = Student.new
+    @student.first_name = params[:first_name]
   end
   
 
